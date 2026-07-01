8f85e5a580501fb4bfede16c7df3d8c9f524693e
Dishank Patel <pateldishank19@gmail.com>
2026-07-01 18:07:01 +0530

    Refactor code structure for improved readability and maintainability

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const errorHandler = require("./middlewares/errorHandler.js");
 const app = express();
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL,
+  origin: true,
   credentials: true,
 };
 
